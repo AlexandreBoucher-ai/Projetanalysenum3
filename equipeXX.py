abded94a6f3696f274e4b0fcf80da28f518a43b3
Alexandre Boucher <albou631@ulaval.ca>
2026-03-09 20:13:49 -0400

    newton début

--- equipeXX.py
+++ equipeXX.py
@@ -4,6 +4,8 @@ from time import time as time
 from reglinA import reglinA
 from reglinB import reglinB
 from newton import newton
+from scipy.linalg import lu
+from numpy.linalg import solve, norm
 
 X = np.loadtxt("points.txt") #Converti le texte en array
 
@@ -40,31 +42,3 @@ plt.title("Figure 2: Tracer des points et des 2 droites de régression")
 plt.xlabel("xi")
 plt.ylabel("yi")
 plt.show()
-
- # e)
- # modification de relinA pour marcher avec la fonction Newton
-def reglinA(X) :
-    '''
-    Args:
-        X : le jeu de donnees n x 2
-
-    Returns :
-        beta : vecteur des coefficients de la droite de regression
-    '''
-    n = X.shape[0] # Nombre de points
-
-    # Creation du membre de droite
-    y = X[:,1].reshape(n,1)
-
-    # Creation de la matrice A
-    A = np.hstack((np.ones((n,1)), X[:,0].reshape(n,1)))
-
-    # Resolution du systeme rectangulaire (approche A)
-    AtA = A.T@A
-    L, U = lu(AtA, permute_l=True)
-    z = solve(L,A.T@y)
-    beta = solve(U,z)
-
-    print(f"Norme du residu ||F(beta)|| = ||A*beta - y|| = {norm(A@beta-y)}")
-
-    return beta
--- newton.py
+++ newton.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.linalg import solve, norm
 import scipy.linalg as la
+from scipy.linalg import lu
 
 def newton(beta_init, F, J, tol, nmax):
     '''
@@ -8,29 +9,41 @@ def newton(beta_init, F, J, tol, nmax):
         beta_init : point initial (vecteur colonne 3x1)
         F         : fonction vectorielle (vecteur colonne nx1) dont on cherche le zero. DEPEND DE BETA.
         J         : matrice jacobienne de F (matrice nxn). DEPEND DE BETA.
+        # n n,est pas le nb de points ici, c'est mêlant
         tol       : tolerance pour determiner la convergence
         nmax      : nombre maximal d'iterations
 
     Returns:
         beta      : vecteur des coefficients de la courbe de regression
     '''
-
+    # Préparation itération
+    # on pose beta = beta0
     beta = beta_init;
     assert beta.shape == (3,1), "beta_init doit être un vecteur colonne!"
     n = 0;
+    # on poe beta0 = la variable d'accumulation
     res = norm(F(beta.flatten()));
+    # dbeta = delta beta = variation de beta
+    # on initialise l'erreur sur beta à infini
     dbeta = np.inf;
-
+    # on fait l'itération de newton
+    # on arrête l'itération lorsque dbeta est plus grand que la tolérance pour la divergeance
     while res > tol and norm(dbeta) > tol and n < nmax :
+        # cacul le jacobien et la fonction en fonction de beta
         Jb = J(beta.flatten());
         Fb = F(beta.flatten());
         assert Fb.shape == (1000,1), "F doit retourner un vecteur colonne!"
-
         # Resolution du systeme pour calculer la correction
         # Calcul de dbeta en utilisant approche (A) ou (B)
-        # Modifier ici !
+        # MODIFIER ICI !
+        # rappel dbeta = -(Fb/Jb) ne fonctionne pas!
 
+
+
+
+   
         #dbeta = ...
+
 
         # Appliquer la correction
         assert dbeta.shape == (3,1), "dbeta doit être un vecteur colonne!"
